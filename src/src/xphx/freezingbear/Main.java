cc1084c7df5e4419068f0966e19413e795176b60
Kyle Fahringer <jxpheonix@gmail.com>
2012-08-07 16:13:07 -0400

    freeze the bear
    but you can't freeze a polar bear

--- src/src/xphx/freezingbear/Main.java
+++ src/src/xphx/freezingbear/Main.java
@@ -1,13 +1,32 @@
 package xphx.freezingbear;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 public class Main {
+	
+	//this is probably the worst code ever
+	
+	boolean banana = false;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("freezing-bear");
+		Main mainification = new Main(); //it's been mainified!
+		mainification.FreezeTheBear(); //i don't like static classes >:(
 
+	}
+
+	private void FreezeTheBear() {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton button = new JButton("Freeze the bear");
+		button.setToolTipText("But you can't freeze a polar bear!");
+		frame.add(button);
+		frame.pack();
+		frame.setVisible(true);
+		banana = true;
 	}
 
 }
